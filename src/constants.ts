74fc267d05337a475145371d4372b9b384000a0b
Daniel Zhang <dzhan96@uwo.ca>
2020-02-20 18:10:40 -0500

    Remove unused enums

--- src/constants.ts
+++ src/constants.ts
@@ -1,26 +1,3 @@
-export enum Attributes {
-  BOLD = 'b',
-  ITALIC = 'i',
-  UNDERLINE = 'u',
-  STRIKE = 's',
-  FONT = 'f',
-  FONT_SIZE = 'fs',
-  COLOR = 'c',
-  BACKGROUND_COLOR = 'bc',
-  ENTITY_SENTINEL = 'ent',
-  LINE_SENTINEL = 'l',
-  LINE_INDENT = 'li',
-  LINE_ALIGN = 'la',
-  LIST_TYPE = 'lt',
-}
-
-export enum Sentinels {
-  // A special character we insert at the beginning of lines so we can attach attributes to it to represent "line attributes."  E000 is from the unicode "private use" range.
-  LINE_CHARACTER = '\uE000',
-  // A special character used to represent any "entity" inserted into the document (e.g. an image).
-  ENTITY_CHARACTER = '\uE001',
-}
-
 export type MonacoCursor = {
   /** Starting Position of the Cursor */
   position?: number;
